e331ee3333f0a0cc89364626e8f5dac70250e5af
twkang43 <twkang43@gmail.com>
2024-05-13 22:38:45 +0900

    fix bugs on inspection codes

--- data/inspection.py
+++ data/inspection.py
@@ -29,9 +29,10 @@ def parse_args() -> argparse.Namespace:
     return args
 
 def make_autopct(values):
+    value_iter = iter(values)
     def my_autopct(pct):
-        absolute = int(pct/100.*sum(values))    
-        return "{:.1f}%\n({:d})".format(pct, absolute)
+        value = next(value_iter)
+        return f"{pct:.1f}%\n({value:d})"
     return my_autopct
 
 # Load dataset
@@ -70,10 +71,26 @@ class Inspection:
         plt.savefig(os.path.join(os.getcwd(), "data", "results", filename))
         plt.close()
 
+    def draw_bar(self, data, title, filename):
+        data_counter = Counter(list(data.values()))
+
+        plt.figure(figsize=(8, 5))
+        bar = plt.bar(data_counter.keys(), data_counter.values(), color='blue', edgecolor='black', alpha=0.7)
+        plt.suptitle(title, fontweight='bold', fontsize=14)
+        plt.xticks(range(1, 15))
+        plt.xlabel("Number of questions")
+
+        # Annotate the counts above the bars
+        for i, rect in enumerate(bar):
+            height = rect.get_height()
+            plt.text(rect.get_x() + rect.get_width() / 2.0, height, str(int(height)), ha='center', va='bottom')
+
+        plt.savefig(os.path.join(os.getcwd(), "data", "results", filename))
+        plt.close()
+
     def inspect(self):
         self.overall_inspection()
         self.questions_by_category()
-        self.questions_per_setting()
         self.do_extra_answers_exist()
         self.which_episodes_were_used()
         
@@ -88,9 +105,6 @@ class Inspection:
         # Draw a pie chart (Questions by Category)
         self.draw_pie_chart(category_counts, f"Questions by Category", "questions_by_category.svg")
 
-    def questions_per_setting(self):
-        pass
-
     def do_extra_answers_exist(self):
         has_extra_answers = Counter("true" if "extra_answers" in item else "false" for item in self.dataset)
 
@@ -100,10 +114,8 @@ class Inspection:
     def which_episodes_were_used(self):
         used_episodes = Counter([item["episode_history"] for item in self.dataset])
 
-        print("-------- Which episodes were used --------")
-        for key, value in used_episodes.items():
-            print(f"{key}: {value}")
-        print("------------------------------------------")
+        # Draw a bar chart (The number of questions per episode)
+        self.draw_bar(used_episodes, f"The number of questions per episode", "which_episodes_were_used.svg")
 
 def main(args: argparse.Namespace):
     if args.dataset == "hm3d":
