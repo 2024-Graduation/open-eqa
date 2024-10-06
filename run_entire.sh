7bbb6d4ac82087310faead98b49f1e745ab7d716
2021006317 <eve7777@hanyang.ac.kr>
2024-10-06 11:50:54 +0900

    FEAT: shell script

---
+++ run_entire.sh
@@ -0,0 +1,32 @@
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=15 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=10 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=15 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=10 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=15 --output-directory=data/results_4
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=10 --output-directory=data/results_4
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=15 --output-directory=data/results_5
+# python3 openeqa/baselines/gpt4o-origin.py --num-frames=10 --output-directory=data/results_5
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=5 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=7 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=9 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=5 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=7 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=9 --output-directory=data/results_2
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=5 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=7 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=9 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=5 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=7 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=9 --output-directory=data/results_3
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=5 --output-directory=data/results_4
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=7 --output-directory=data/results_4
+# python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=9 --output-directory=data/results_4
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=5 --output-directory=data/results_4
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=7 --output-directory=data/results_4
+# python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=9 --output-directory=data/results_4
+python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=5 --output-directory=data/results_5
+python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=7 --output-directory=data/results_5
+python3 openeqa/baselines/gpt4o.py --num-frames=10 --length-limit=9 --output-directory=data/results_5
+python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=5 --output-directory=data/results_5
+python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=7 --output-directory=data/results_5
+python3 openeqa/baselines/gpt4o.py --num-frames=15 --length-limit=9 --output-directory=data/results_5
